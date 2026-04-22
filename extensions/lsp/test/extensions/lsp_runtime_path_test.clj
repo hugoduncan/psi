0291b70e86fef90ed0376743b3c9596f3d80e8a0
Hugo Duncan <hugo@hugoduncan.org>
2026-04-22 16:16:05 -0400

    fix(test): stabilize lsp didchange diagnostics timing

--- extensions/lsp/test/extensions/lsp_runtime_path_test.clj
+++ extensions/lsp/test/extensions/lsp_runtime_path_test.clj
@@ -307,7 +307,7 @@
                                                 :tool-result {:effects [{:path file-path}]}
                                                 :config {:command lsp-fixture-command
                                                          :startup-timeout-ms 2000
-                                                         :diagnostics-timeout-ms 2000}})
+                                                         :diagnostics-timeout-ms 5000}})
           root (:workspace-root second-result)
           svc (services/service-in ctx (sut/workspace-key root))
           debug @(:debug-atom svc)]
