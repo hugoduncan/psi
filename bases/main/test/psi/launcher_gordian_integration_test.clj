413c7f4bdebb831ed4e2e68433d6544a5916d8f3
Hugo Duncan <hugo@hugoduncan.org>
2026-04-24 16:23:41 -0400

    ⚒ add Gordian launcher manifest proof

---
+++ bases/main/test/psi/launcher_gordian_integration_test.clj
@@ -0,0 +1,29 @@
+(ns psi.launcher-gordian-integration-test
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [psi.launcher :as launcher]))
+
+(def psi-root
+  "/Users/duncan/projects/hugoduncan/psi/fix-extensions")
+
+(def gordian-cwd
+  "/Users/duncan/projects/hugoduncan/gordian/gordian-master")
+
+(deftest ^:integration launch-plan-expands-gordian-psi-owned-empty-map-manifest-entries
+  (testing "real Gordian manifest expands workflow-loader into launcher basis deps"
+    (let [plan          (launcher/launch-plan [] gordian-cwd psi-root :installed)
+          workflow-dep  (get-in plan [:basis :deps 'psi/workflow-loader])
+          mementum-dep  (get-in plan [:basis :deps 'psi/mementum])
+          manifest-deps (get-in plan [:manifest-info :expanded-manifest :deps])]
+      (is (= {:local/root (str psi-root "/extensions/workflow-loader")}
+             workflow-dep))
+      (is (= {:local/root (str psi-root "/extensions/mementum")}
+             mementum-dep))
+      (is (= 'extensions.workflow-loader/init
+             (get-in manifest-deps ['psi/workflow-loader :psi/init])))
+      (is (= 'extensions.mementum/init
+             (get-in manifest-deps ['psi/mementum :psi/init])))
+      (is (some #{'psi/workflow-loader} (:defaulted-libs (:manifest-info plan))))
+      (is (some #{'psi/workflow-loader} (:inferred-init-libs (:manifest-info plan))))
+      (is (= ["clojure" "-Sdeps"] (subvec (:command plan) 0 2)))
+      (is (= ["-M" "-m" "psi.main"] (subvec (:command plan) 3 6))))))
